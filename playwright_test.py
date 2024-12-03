479d8a7c0b3b3c63c0f0d6a3ec11bad39e1bd207
Andrew Masek <andrewmasek@gmail.com>
2024-12-02 19:19:10 -0800

    Username and password auth

--- playwright_test.py
+++ playwright_test.py
@@ -1,6 +1,8 @@
 import os
 import time 
 from playwright.sync_api import sync_playwright
+from dotenv import load_dotenv
+
 
 def login_to_website():
     with sync_playwright() as p:
@@ -12,6 +14,19 @@ def login_to_website():
         # Navigate to the login page
         page.goto("https://canvas.ucsd.edu/courses/59004/external_tools/5826")
 
+        # Load environment variables from .env file
+        load_dotenv()
+
+        # Get the username from the environment variable
+        username = os.getenv('CANVAS_USERNAME')
+        password = os.getenv('CANVAS_PASSWORD')
+        print(username, password)
+        # Fill in the username textbox
+        page.fill('input[type="username"]', username)
+        page.fill('input[type="password"]', password)
+        # Click the submit button
+        page.click('button[type="submit"]')
+        
         # page.wait_for_timeout(timeout=10000)
         # page.wait_for_selector() # apparently this function is discouraged: https://playwright.dev/python/docs/api/class-page
         # page.wait_for_url('https://canvas.ucsd.edu/courses/59004/external_tools/5826)')
